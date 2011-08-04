74ef293bfac910484f8bcd34d103e8c7744bf02a
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2011-08-04 08:45:09 -0400

    Replaced for loop with map and lambda function for file search

--- discover.py
+++ discover.py
@@ -3,14 +3,19 @@
 
 import argparse, os, re
 
+
+def discoverFile(path, fileName, comparer):
+	if comparer(fileName):
+		print "/".join(path+[fileName])
+
 def discover(path, query, comparer):
 	for(path, dirs, files) in os.walk(path):
 		path = path.split('/')
 		if comparer(path[len(path)-1]):
 			print "/".join(path)
-		for file in files:
-			if comparer(file):
-				print "/".join(path+[file])
+
+		df = lambda x: discoverFile(path,x,comparer)	
+		map(df,files) 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Search file system')
